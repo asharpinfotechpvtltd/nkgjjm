76586b19fad4e3ce91bb9f4a36ca5e27533bd9a2
somil lohani <somillohani@hotmail.com>
2022-06-09 11:10:44 +0530

    second

---
+++ Api/WarehousesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nkgjjm.Models;
+
+namespace Nkgjjm.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehousesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WarehousesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Warehouses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Warehouse>>> GetTblWarehouse()
+        {
+          if (_context.TblWarehouse == null)
+          {
+              return NotFound();
+          }
+            return await _context.TblWarehouse.ToListAsync();
+        }
+
+        // GET: api/Warehouses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Warehouse>> GetWarehouse(int id)
+        {
+          if (_context.TblWarehouse == null)
+          {
+              return NotFound();
+          }
+            var warehouse = await _context.TblWarehouse.FindAsync(id);
+
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            return warehouse;
+        }
+
+        // PUT: api/Warehouses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWarehouse(int id, Warehouse warehouse)
+        {
+            if (id != warehouse.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(warehouse).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WarehouseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Warehouses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Warehouse>> PostWarehouse(Warehouse warehouse)
+        {
+          if (_context.TblWarehouse == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.TblWarehouse'  is null.");
+          }
+            _context.TblWarehouse.Add(warehouse);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWarehouse", new { id = warehouse.Id }, warehouse);
+        }
+
+        // DELETE: api/Warehouses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteWarehouse(int id)
+        {
+            if (_context.TblWarehouse == null)
+            {
+                return NotFound();
+            }
+            var warehouse = await _context.TblWarehouse.FindAsync(id);
+            if (warehouse == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblWarehouse.Remove(warehouse);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool WarehouseExists(int id)
+        {
+            return (_context.TblWarehouse?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
